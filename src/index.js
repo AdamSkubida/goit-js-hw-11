65a17943227f57007aae70967943455127b28984
AdamSkubida <adasiow.s@gmail.com>
2023-06-02 17:26:45 +0200

    generating photo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-import { getURL } from './fetch';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
+
+import axios from 'axios';
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const q = document.querySelector('[name="searchQuery"]').value;
+  console.log(q);
+
+  async function getPhotos() {
+    try {
+      const apiKey = '36950464-5a13f55b57e77fe0085f04ef4';
+      const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${q}&per_page=40`;
+
+      const response = await axios.get(apiUrl);
+      const photos = response.data.hits;
+
+      console.log(photos);
+
+      // Przetwarzanie i wyświetlanie zdjęć na stronie
+      photos.forEach(photo => {
+        const image = document.createElement('img');
+        image.src = photo.webformatURL;
+        gallery.appendChild(image);
+      });
+    } catch (error) {
+      Notify.failure(error);
+    }
+  }
+
+  // Wywołanie funkcji pobierającej zdjęcia
+  getPhotos();
+});
